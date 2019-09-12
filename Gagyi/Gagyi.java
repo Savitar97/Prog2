b1d1514b0c7441b2693a6c038858deae185ecff0
Attila Tóth <43468256+Savitar97@users.noreply.github.com>
2019-09-12 18:14:13 +0200

    Add files via upload

---
+++ Gagyi/Gagyi.java
@@ -0,0 +1,11 @@
+public class Gagyi{
+
+public static void main(String[] args) {
+	
+	Integer x = 127;
+	Integer t = 57;
+	while (x <= t && x >= t && t != x)
+		{ System.out.println("Gagyi");  }
+	System.out.println("Gagyi vége"); 
+}
+}
