fd36bbc2b539da87150d803c9bd7c240dae6deea
Attila Tóth <43468256+Savitar97@users.noreply.github.com>
2019-09-16 12:36:57 +0200

    Update nemesis.java

--- WebApplication1/src/java/company/nemesis/servlets/nemesis.java
+++ WebApplication1/src/java/company/nemesis/servlets/nemesis.java
@@ -21,7 +21,7 @@ public class nemesis extends HttpServlet {
 
    @Override
 public void doGet(HttpServletRequest request, HttpServletResponse response)
-throws ServletException, IOException {
+throws ServletException, IOException { 
    
 String bemenet = request.getParameter("text");
 byte[] bytes = bemenet.getBytes();
@@ -29,32 +29,38 @@ LZWBinFa binFa = new LZWBinFa();
 java.io.PrintWriter kiFile =new java.io.PrintWriter(new java.io.BufferedWriter(new java.io.FileWriter("output.txt")));
 
 boolean kommentben = false;
-for(int j = 0; j < bytes.length; ++j) {
-if (bytes[j] == 0x3e) {
-kommentben = true;
-continue;
-}
-if (bytes[j] == 0x0a) {
-kommentben = false;
-continue;
-}
-if (kommentben) {
-continue;
-}
-if (bytes[j] == 0x4e)
+for(int j = 0; j < bytes.length; ++j) 
 {
-continue;
-}
-for (int i = 0; i < 8; ++i) {
-if ((bytes[j] & 0x80) != 0)
-{
-binFa.egyBitFeldolg('1');
-} else
-{
-binFa.egyBitFeldolg('0');
-}
-bytes[j] <<= 1;
-}
+   if (bytes[j] == 0x3e)
+   {
+      kommentben = true;
+      continue;
+   }
+   if (bytes[j] == 0x0a)
+   {
+      kommentben = false;
+      continue;
+   }
+   if (kommentben) 
+   {
+      continue;
+   }
+   if (bytes[j] == 0x4e)
+   {
+      continue;
+   }
+   for (int i = 0; i < 8; ++i) 
+   {
+      if ((bytes[j] & 0x80) != 0)
+      {
+         binFa.egyBitFeldolg('1');
+      }
+      else
+      {
+         binFa.egyBitFeldolg('0');
+      }
+      bytes[j] <<= 1;
+   }
 }
    
    
@@ -74,10 +80,12 @@ out.println("<html>");
 out.println("<head><title>BinfaServlet</title></head>");
 out.println("<body>");
 out.println("<h1>Binfa</h1>");
-try(BufferedReader br = new BufferedReader(new InputStreamReader(finstream))) {
-for(String line; (line = br.readLine()) != null; ) {
-out.println("<p>" + line + "</p>");
-}
+try(BufferedReader br = new BufferedReader(new InputStreamReader(finstream))) 
+{
+   for(String line; (line = br.readLine()) != null; )
+   {
+      out.println("<p>" + line + "</p>");
+   }
 }
 out.println("</body></html>");
 }
