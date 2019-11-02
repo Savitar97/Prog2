7941cfeced683085d6c3f16a1a4855b957e4833d
Tóth Attila <atoth1571@gmail.com>
2019-11-02 01:29:40 +0100

    update

--- Map/map.cpp
+++ Map/map.cpp
@@ -7,25 +7,33 @@
 #include <functional>
 #include <array>
 
-std::vector<std::pair<std::string, int>> sort_map ( std::map <std::string, int> &rank )
+std::vector<std::pair<std::string, int>> rendezMap ( std::map <std::string, int> &rank )
 {
-        std::vector<std::pair<std::string, int>> ordered;
+        std::vector<std::pair<std::string, int>> rendezett;
 
         for ( auto & i : rank ) {
                 if ( i.second ) {
                         std::pair<std::string, int> p {i.first, i.second};
-                        ordered.push_back ( p );
+                        
+                        rendezett.push_back ( p );
+
                 }
-        }
+        };
 
         std::sort (
-                std::begin ( ordered ), std::end ( ordered ),
+                std::begin ( rendezett ), std::end ( rendezett ),
         [ = ] ( auto && p1, auto && p2 ) {
+
+        		
+        		std::cout<<p1.second<<std::endl;
+        		std::cout<<p2.second<<std::endl;
+        		std::cout<<"========="<<std::endl;
                 return p1.second > p2.second;
+                
         }
         );
 
-        return ordered;
+        return rendezett;
 }
 
 
@@ -34,20 +42,23 @@ std::vector<std::pair<std::string, int>> sort_map ( std::map <std::string, int> 
 int main()
 {
 
-    std::map<std::string, int> mapOfWords;
-    mapOfWords.insert(std::make_pair("earth", 4));
-    mapOfWords.insert(std::make_pair("moon", 2));
-    mapOfWords.insert(std::make_pair("jupiter", 10));
-    mapOfWords.insert(std::make_pair("mars", 7));
-    mapOfWords.insert(std::make_pair("saturnus", 9));
-    std::map<std::string, int>::iterator it = mapOfWords.begin();
-    while(it != mapOfWords.end())
+    std::map<std::string, int> planetsMap;
+    planetsMap.insert(std::make_pair("earth", 4));
+    planetsMap.insert(std::make_pair("moon", 2));
+    planetsMap.insert(std::make_pair("jupiter", 10));
+    planetsMap.insert(std::make_pair("mars", 7));
+    planetsMap.insert(std::make_pair("uranus", 3));
+    planetsMap.insert(std::make_pair("saturnus", 9));
+    std::map<std::string, int>::iterator it = planetsMap.begin();
+    while(it != planetsMap.end())
     {
         std::cout<<it->first<<" :: "<<it->second<<std::endl;
         it++;
     }
-      std::vector<std::pair<std::string, int>> rendezett=sort_map(mapOfWords);
-      std::cout<<
+    std::cout<<"-------------------------------------------------------------"<<std::endl;
+      std::vector<std::pair<std::string, int>> rendezett=rendezMap(planetsMap);
+      std::cout<<"-------------------------------------------------------------"<<std::endl;
+      std::cout<<"Rendezés után:"<<std::endl;
     for ( auto & i : rendezett )
                 std::cout << i.first << " - "  << i.second << std::endl;
     return 0;
