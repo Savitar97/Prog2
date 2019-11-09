609428c10d457b21234bc73df769259d2f905398
Attila Tóth <43468256+Savitar97@users.noreply.github.com>
2019-11-09 22:05:15 +0100

    Add files via upload

---
+++ src/main/java/com/example/porospin/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.porospin;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView yasuo;
+    private Random random = new Random();
+    private int lastDir;
+    private boolean spinning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        yasuo = findViewById(R.id.yasu);
+    }
+
+    public void spinToWin(View v) {
+        if (!spinning) {
+            int newDir = random.nextInt(1800);
+            float pivotX = yasuo.getWidth() / 2;
+            float pivotY = yasuo.getHeight() / 2;
+
+            Animation rotate = new RotateAnimation(lastDir, newDir, pivotX, pivotY);
+            rotate.setDuration(2500);
+            rotate.setFillAfter(true);
+            rotate.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    spinning = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    spinning = false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            lastDir = newDir;
+            yasuo.startAnimation(rotate);
+        }
+    }
+}
